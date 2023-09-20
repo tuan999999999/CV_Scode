f695926b3a91475ee74bbded7b4a946530f43e92
tuan999999999 <75604747+tuan999999999@users.noreply.github.com>
2023-09-20 21:20:43 +0900

    Delete CV_SCode directory

--- CV_SCode/test.py
+++
@@ -1,9 +0,0 @@
-import cv2 as cv
-import sys
-img = cv.imread("a1.jpg")
-if img is None:
-    sys.exit("Could not read the image.")
-cv.imshow("Display window", img)
-k = cv.waitKey(0)
-if k == ord("s"):
-    cv.imwrite("a1.png", img)
